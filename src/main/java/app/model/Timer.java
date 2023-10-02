ddb6b2707f99027e0ab7f051ddc3f5048928785e
ZemtsH <zemtsovskaya.ov@gmail.com>
2023-10-02 17:12:15 +0300

    update timer

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,10 +1,14 @@
 package app.model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Scope;
 @Component
+@Scope("singleton")
 public class Timer {
 
-    private Long nanoTime = System.nanoTime();
+    private static final Long nanoTime = System.nanoTime();
 
     public Long getTime() {
         return nanoTime;
